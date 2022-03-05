295c30550d722efa4e553ded3e4f590e982e6553
Beta Folf <beta@betafolf.dev>
2022-03-05 14:53:51 -0600

    feat: html meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,45 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { NextPage } from 'next';
+import App, { AppContext, AppProps } from 'next/app';
+import Head from 'next/head';
+
 import { theme } from '~/constants/themes';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+interface PageProps {
+  title: string;
+}
+type CustomAppProps<T> = AppProps<T> & {
+  Component: NextPage;
+};
+
+function MyApp({ Component, pageProps }: CustomAppProps<PageProps>) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>{pageProps.title}</title>
+        <meta name="author" content="Beta Folf" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="OwO what's this. I'm a gay furry full stack web developer. I clearly make great life choices."
+        />
+        <meta name="keywords" content="beta, folf, furry, gay, owo" />
+        <meta name="theme-color" content="#4379c0" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  return appProps;
+};
 
 export default MyApp;
